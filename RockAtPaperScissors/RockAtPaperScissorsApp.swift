ced12880808bec40a4a4d2490dc1c86ffd8e0be9
KarolinaGroszewska <karolina.groszewska@gmail.com>
2023-01-12 10:57:33 -0600

    Initial Commit

---
+++ RockAtPaperScissors/RockAtPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockAtPaperScissorsApp.swift
+//  RockAtPaperScissors
+//
+//  Created by Kari on 1/12/23.
+//
+
+import SwiftUI
+
+@main
+struct RockAtPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
